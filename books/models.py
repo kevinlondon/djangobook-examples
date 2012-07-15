c432c9b0d64649e98d7477d320e8421698f6ea18
Kaedon <kevinlondon@gmail.com>
2012-07-14 23:36:55 -0700

    End of Chapter 6: Admin site and cleanup.

--- books/models.py
+++ books/models.py
@@ -14,7 +14,7 @@ class Publisher(models.Model):
 class Author(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=40)
-    email = models.EmailField()
+    email = models.EmailField(blank=True, verbose_name='e-mail')
 
     def __unicode__(self):
         return u'%s %s' % (self.first_name, self.last_name)
@@ -23,7 +23,7 @@ class Book(models.Model):
     title = models.CharField(max_length=100)
     authors = models.ManyToManyField(Author)
     publisher = models.ForeignKey(Publisher)
-    publication_date = models.DateField()
+    publication_date = models.DateField(blank=True, null=True)
 
     def __unicode__(self):
         return self.title
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import patterns, include, url
 from mysite.views import hello, current_datetime, hours_ahead
-
+from django.contrib import admin
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
@@ -14,8 +14,8 @@ urlpatterns = patterns('',
     
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+      url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+      url(r'^admin/', include(admin.site.urls)),
 )
